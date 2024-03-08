6664923f9af4733509b186721319d330e9c8375b
AFPedreros <felipe.pedreros94@gmail.com>
2024-03-08 15:52:31 -0500

    Add prettier configuration and global styles

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { Toaster } from "sonner";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      {children}
+      <Toaster richColors />
+    </NextUIProvider>
+  );
+}
