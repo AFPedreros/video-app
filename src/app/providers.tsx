12ddff935b8f894accf6c3d05df2b876a3112903
AFPedreros <felipe.pedreros94@gmail.com>
2024-03-08 16:10:50 -0500

    Add NextThemesProvider to Providers component and update tailwind.config.ts

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      {children}
-      <Toaster richColors />
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+        <Toaster richColors />
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
